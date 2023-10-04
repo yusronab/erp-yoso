4daf926bc6cf37edb2d7f3a8e6c88e6ed726be80
yusronab <yusron.arly@gmail.com>
2023-10-04 14:23:22 +0700

    migrate repo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth"
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
