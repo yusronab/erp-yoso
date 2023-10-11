676e29339dddf0db8e1f89a91bd3ee1a13b52ff8
yusronab <yusron.arly@gmail.com>
2023-10-11 16:39:38 +0700

    api post company

--- app/(admin)/company/list.tsx
+++ app/(admin)/company/list.tsx
@@ -1,27 +1,34 @@
 import moment from "moment";
 import Image from "next/image";
 import 'moment/locale/id';
+import { BiSolidEdit } from "react-icons/bi";
 
 const ListComponent = ({ datas }: { datas: any[] }) => {
     return (
         <div className="grid grid-cols-3">
             {datas.length !== 0 ? (
                 datas.map((item, i) => (
-                    <div key={i} className="p-3 rounded-md border border-gray-300 hover:shadow-lg">
-                        <div className="flex">
-                            <div className="avatar">
-                                <div className="w-8 rounded-full bg-gray-300 object-cover">
-                                    <Image src={item.image} alt="logo" style={{ objectFit: "cover" }}/>
+                    <div className="relative group">
+                        <div key={i} className="p-3 rounded-md border border-gray-300 hover:shadow-lg">
+                            <div className="flex">
+                                <div className="avatar">
+                                    <div className="w-8 h-8 rounded-full bg-gray-300">
+                                        <Image src={item.image} alt="logo" style={{ objectFit: "cover" }} />
+                                    </div>
+                                </div>
+                                <div className="ml-3">
+                                    <p className="font-bold">{item.name}</p>
+                                    <p className="text-sm text-gray-500 capitalize">{item.city}</p>
                                 </div>
                             </div>
-                            <div className="ml-3">
-                                <p className="font-bold">{item.name}</p>
-                                <p className="text-sm text-gray-500 capitalize">{item.city}</p>
-                            </div>
+                            <p className="mt-8">
+                                {`Diperbarui : ${moment(item.updatedAt).startOf('hour').fromNow()}`}
+                            </p>
                         </div>
-                        <p className="mt-8">
-                            {`Diperbarui : ${moment(item.updatedAt).startOf('hour').fromNow()}`}
-                        </p>
+                        <button className="hidden absolute top-0 right-0 p-2 mt-2 mr-2 bg-blue-500
+                        text-white rounded-full group-hover:block">
+                            <BiSolidEdit size={16} />
+                        </button>
                     </div>
                 ))
             ) : (
